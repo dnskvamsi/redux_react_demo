73268e25ee3a8f3b6b42973479857950ed91c708
NSKVamsi Devarapalli <naga.devarapalli.18@mountblue.tech>
2022-03-14 18:51:00 +0530

    modified loading component style

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -3,10 +3,11 @@ import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "./ProductCard";
 import Grid from "@mui/material/Grid";
 import axios from "axios";
+import Loading from "./Loading";
 import { setProducts } from "../redux/actions/productAction";
 
 export default function ProductListing() {
-  const products = useSelector((state) => state);
+  const products = useSelector((state) => state.allProducts.products);
   const dispatch = useDispatch();
   const fetchProducts = async () => {
     const response = await axios
@@ -19,15 +20,20 @@ export default function ProductListing() {
   React.useEffect(() => {
     fetchProducts();
   }, []);
-  return (
-    <Grid
-      container
-      rowSpacing={3}
-      columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-      direction="row"
-      style={{ textAlign: "center" }}
-    >
-      <ProductCard />
-    </Grid>
-  );
+
+  if (products.length === 0) {
+    return <Loading />;
+  } else {
+    return (
+      <Grid
+        container
+        rowSpacing={3}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+        direction="row"
+        style={{ textAlign: "center" }}
+      >
+        <ProductCard />
+      </Grid>
+    );
+  }
 }
